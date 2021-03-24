733adbf70c0147345435dde8c617ef4ae1f2a79f
Developsteven <steven.perez099@gmail.com>
2021-03-24 17:17:14 -0500

    Modificando clienteDao para poder manejar paginacion

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -9,6 +9,8 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -37,6 +39,12 @@ public class ClienteRestController {
 	@GetMapping("/clientes")
 	private List<Cliente> index() {
 		return clienteService.findAll();
+	}
+	
+	
+	@GetMapping("/clientes/page/{page}")
+	private Page<Cliente> index(@PathVariable Integer page) {
+		return clienteService.findAll(PageRequest.of(page, 4));
 	}
 
 	@GetMapping("/clientes/{id}")

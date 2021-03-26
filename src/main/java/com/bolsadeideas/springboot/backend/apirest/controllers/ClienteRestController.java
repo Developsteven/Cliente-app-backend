9bb4e6dc4010ee75c8ba959a8b9f1a8e8cd1f3ef
Developsteven <steven.perez099@gmail.com>
2021-03-26 15:47:47 -0500

    Modificando metodo de subida de imagen para eliminar si hay imagen anteior.

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -1,5 +1,6 @@
 package com.bolsadeideas.springboot.backend.apirest.controllers;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -154,6 +155,16 @@ public class ClienteRestController {
 		Map<String, Object> response = new HashMap<>();
 		
 		try {
+			Cliente cliente = clienteService.findById(id);
+			String nombreFotoAnterior = cliente.getFoto();
+			
+			if(nombreFotoAnterior !=null && nombreFotoAnterior.length()>0) {
+				Path rutaFotoAnterior = Paths.get("uploads").resolve(nombreFotoAnterior).toAbsolutePath();
+				File archivoFotoAnterior =rutaFotoAnterior.toFile();
+				if(archivoFotoAnterior.exists() && archivoFotoAnterior.canRead()) {
+					archivoFotoAnterior.delete();
+				}
+			}
 			clienteService.delet(id);
 		} catch (DataAccessException e) {
 			response.put("mensaje", "Error al eliminar el cliente en la base de datos");
@@ -180,6 +191,16 @@ public class ClienteRestController {
 				response.put("mensaje", "Error al subir la imagen del cliente " + nombreArchivo);
 				response.put("error", e.getMessage().concat(": ").concat(e.getCause().getMessage()));
 				return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+			
+			String nombreFotoAnterior = cliente.getFoto();
+			
+			if(nombreFotoAnterior !=null && nombreFotoAnterior.length()>0) {
+				Path rutaFotoAnterior = Paths.get("uploads").resolve(nombreFotoAnterior).toAbsolutePath();
+				File archivoFotoAnterior =rutaFotoAnterior.toFile();
+				if(archivoFotoAnterior.exists() && archivoFotoAnterior.canRead()) {
+					archivoFotoAnterior.delete();
+				}
 			}
 			
 			cliente.setFoto(nombreArchivo);

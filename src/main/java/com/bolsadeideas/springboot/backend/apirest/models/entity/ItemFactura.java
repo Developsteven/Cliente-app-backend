db44fb3d65876eec7043e70e41dca0c800ca1a99
Developsteven <steven.perez099@gmail.com>
2021-04-12 17:08:18 -0500

    Creando clase Producto.

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/ItemFactura.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/ItemFactura.java
@@ -3,9 +3,11 @@ package com.bolsadeideas.springboot.backend.apirest.models.entity;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -18,6 +20,8 @@ public class ItemFactura implements Serializable {
 	
 	private Integer cantidad;
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Producto producto;
 	
 	public Long getId() {
 		return id;
@@ -35,9 +39,19 @@ public class ItemFactura implements Serializable {
 		this.cantidad = cantidad;
 	}
 	
-	public Double calcularCantidad() {
-		return cantidad.doubleValue();
+	public Double getImporte() {
+		return cantidad.doubleValue()*producto.getPrecio();
 	}
+	
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+
 
 	private static final long serialVersionUID = 1L;
 
